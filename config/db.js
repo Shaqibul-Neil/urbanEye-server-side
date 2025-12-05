d63dd645c44f70a4654e9135155cf77758192396
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-12-05 20:42:35 +0600

    first commit

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+const client = new MongoClient(process.env.DATABASE_URL, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const dbName = "urbanEyeDB";
+async function connectDB() {
+  console.log("MongoDB connected successfully");
+  const db = client.db(dbName);
+  return {
+    userCollection: db.collection("users"),
+  };
+}
+
+module.exports = { client, connectDB };
---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { connectDB } = require("./config/db");
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+async function run() {
+  try {
+    const collections = await connectDB();
+    //Routes
+  } catch (err) {
+    console.error("Database connection failed:", err.message);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("UrbanEye Server");
+});
+
+app.listen(port, () => {
+  console.log(`UrbanEye Server listening on port ${port}`);
+});
